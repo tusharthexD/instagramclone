235cfacd9333b445f026523de38cc27c367e9426
Tushar <tusharsuthar6@gmail.com>
2024-04-10 14:03:07 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server :{
-    proxy : {
-      "/api" : "https://instaclonebe-rfqu.onrender.com"
+    proxy: {
+      '/api': {
+        target: 'https://instaclonebe-rfqu.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
   },
   plugins: [react()],
