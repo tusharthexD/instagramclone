c6955fa59efe8da9a3c8bc7405e524ff9908e0f4
Tushar <tusharsuthar6@gmail.com>
2024-04-11 13:12:13 +0530

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server :{
-    proxy: {
-      '/api': {
-        target: 'https://instaclonebe-rfqu.onrender.com/',
-        changeOrigin: true,
-      }
-    }
-  },
   plugins: [react()],
-})
+  server: {
+      proxy: {
+        '/api': {
+          target: 'http://instaclonebe-rfqu.onrender.com',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+});
