0b57807b02c7d62ac2099336547d96119153fc07
Tushar <tusharsuthar6@gmail.com>
2024-04-13 09:29:47 +0530

    dn

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server :{
-    proxy : {
-      '/api': {
-        target: 'https://instaclonebe-rfqu.onrender.com',
-        changeOrigin: true,
-      },
-    }
-  },
   plugins: [react()],
 })
