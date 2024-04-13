913d2733ce4ec1374b12e5bf229ab7a52c1a4586
Tushar <tusharsuthar6@gmail.com>
2024-04-13 11:27:20 +0530

    d

--- src/Pages/auth/LoginPage.jsx
+++ src/Pages/auth/LoginPage.jsx
@@ -17,7 +17,7 @@ function LoginPage() {
     event.preventDefault();
     await axios.post("https://instaclonebe-rfqu.onrender.com/api/login", login).then((res) => {
       if (res.data.isLoggedin) {
-        // navigate('/')
+        navigate('/')
       }
       console.log(res.data);
       setWrongPsw(res.data.message)
